41e15880cf325da16dffe1bfb833d36064df1f5a
Yixuan Qiu <yixuanq@gmail.com>
2018-10-28 12:52:00 -0400

    convergence test

--- src/fastfps.cpp
+++ src/fastfps.cpp
@@ -35,7 +35,8 @@ inline double lambda_min_thresh(double x, double thresh)
 }
 
 // [[Rcpp::export]]
-List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
+List fastfps(NumericMatrix S, int d, double lambda,
+             int maxiter, double eps_abs, double eps_rel,
              double alpha0, double mu, double r,
              bool exact_feas = false, bool verbose = true)
 {
@@ -68,7 +69,7 @@ List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
     for(int i = 0; i < maxiter; i++)
     {
         time1 = get_wall_time();
-        alpha = alpha0 / std::pow(i + 1.0, 0.75);
+        alpha = alpha0 / (i + 1.0);
 
         // L1 thresholding
         soft_thresh_sparse(x, lambda * alpha, xsp);
@@ -86,6 +87,14 @@ List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
         // Trace shrinkage
         const double tbar = x.diagonal().mean();
         const double tr_shift = double(d) / double(p) - tbar;
+        diag.array() = x.diagonal().array() + tr_shift;
+        const double beta = alpha * mu / std::sqrt(double(p)) / std::abs(tr_shift);
+        if(beta >= 1.0)
+        {
+            x.diagonal().noalias() = diag;
+        } else {
+            x.diagonal().noalias() = (1.0 - beta) * x.diagonal() + beta * diag;
+        }
 
         // Compute (approximate) feasibility loss
         const double feas1 = mu * r * (std::max(0.0, -evals[1]),
@@ -96,15 +105,6 @@ List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
         fn_feas.push_back(feas1 + feas2);
         if(verbose)
             Rcpp::Rcout << i << std::endl;
-
-        diag.array() = x.diagonal().array() + tr_shift;
-        const double beta = alpha * mu / std::sqrt(double(p)) / std::abs(tr_shift);
-        if(beta >= 1.0)
-        {
-            x.diagonal().noalias() = diag;
-        } else {
-            x.diagonal().noalias() = (1.0 - beta) * x.diagonal() + beta * diag;
-        }
 
         // Gradient descent
         x.triangularView<Eigen::Upper>() = x.triangularView<Eigen::Lower>().transpose();
@@ -122,10 +122,12 @@ List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
         }
         xold.noalias() = x;
 
+        // Record elapsed time and objective function values
         time2 = get_wall_time();
         fn_obj.push_back(-Smat.cwiseProduct(x).sum() + lambda * x.cwiseAbs().sum());
         time.push_back(time2 - time1);
 
+        // Compute exact feasibility loss
         if(exact_feas)
         {
             eigs_feas.init();
@@ -140,6 +142,23 @@ List fastfps(NumericMatrix S, int d, double lambda, int maxiter,
             fn_feas2.back() = feas2;
             fn_feas.back() = feas1 + feas2;
         }
+
+        // Convergence test, only after 10 iterations
+        if(i < 10)
+            continue;
+        // Feasibility loss, using the average of the most recent 5 values
+        const double feas_curr = std::accumulate(fn_feas.end() - 5, fn_feas.end(), 0.0) / 5.0;
+        const double feas_prev = std::accumulate(fn_feas.end() - 6, fn_feas.end() - 1, 0.0) / 5.0;
+        const double feas_diff = std::abs(feas_curr - feas_prev);
+        const bool feas_conv = feas_diff < eps_abs || feas_diff < eps_rel * std::abs(feas_prev);
+        // Objective function, using the average of the most recent 5 values
+        const double obj_curr = std::accumulate(fn_obj.end() - 5, fn_obj.end(), 0.0) / 5.0;
+        const double obj_prev = std::accumulate(fn_obj.end() - 6, fn_obj.end() - 1, 0.0) / 5.0;
+        const double obj_diff = std::abs(obj_curr - obj_prev);
+        const bool obj_conv = obj_diff < eps_abs || obj_diff < eps_rel * std::abs(obj_prev);
+
+        if(feas_conv && obj_conv)
+            break;
     }
 
     soft_thresh_sparse(x, lambda * alpha, xsp);
