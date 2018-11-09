ec663cbae3055c0d0ef2d8140026960bf9385188
Yixuan Qiu <yixuanq@gmail.com>
2018-11-08 21:44:34 -0500

    more typedefs

--- inst/include/fastfps.h
+++ inst/include/fastfps.h
@@ -7,8 +7,10 @@ typedef Eigen::MatrixXd MatrixXd;
 typedef Eigen::VectorXd VectorXd;
 typedef Eigen::Map<MatrixXd> MapMat;
 typedef Eigen::Map<VectorXd> MapVec;
+typedef Eigen::Map<const VectorXd> MapConstVec;
 typedef Eigen::Ref<VectorXd> RefVec;
 typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Map<SpMat> MapSpMat;
+typedef Eigen::Map<const SpMat> MapConstSpMat;
 
 #endif  // FASTFPS_PKG_H
--- src/common.h
+++ src/common.h
@@ -7,9 +7,11 @@ typedef Eigen::MatrixXd MatrixXd;
 typedef Eigen::VectorXd VectorXd;
 typedef Eigen::Map<MatrixXd> MapMat;
 typedef Eigen::Map<VectorXd> MapVec;
+typedef Eigen::Map<const VectorXd> MapConstVec;
 typedef Eigen::Ref<VectorXd> RefVec;
 typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Map<SpMat> MapSpMat;
+typedef Eigen::Map<const SpMat> MapConstSpMat;
 
 
 #endif  // FASTFPS_COMMON_H
