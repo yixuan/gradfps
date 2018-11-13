6e80680447d0f6b40bc5f142ecbb9c1a138be40a
Yixuan Qiu <yixuanq@gmail.com>
2018-11-12 20:52:41 -0500

    some symmetric-matrix-related functions

--- src/fastfps.h
+++ src/fastfps.h
@@ -81,5 +81,131 @@ inline void rank2_update_sparse(
     x.add_to(res.data());
 }
 
+// x += alpha*x + beta*y + gamma*z
+// Only the lower triangular part is read and written
+inline void sym_mat_update(
+    int p, double* x, const double* y, const double* z,
+    double alpha, double beta, double gamma
+)
+{
+    double*       x_col_begin = x;
+    const double* x_col_end   = x + p;
+
+    const double* y_col_begin = y;
+    const double* y_col_end   = y + p;
+
+    const double* z_col_begin = z;
+    const double* z_col_end   = z + p;
+
+    for(int j = 0; j < p; j++)
+    {
+        x = x_col_begin + j;
+        y = y_col_begin + j;
+        z = z_col_begin + j;
+
+        for(; x < x_col_end; x++, y++, z++)
+            (*x) += alpha * (*x) + beta * (*y) + gamma * (*z);
+
+        x_col_begin += p;
+        x_col_end   += p;
+
+        y_col_begin += p;
+        y_col_end   += p;
+
+        z_col_begin += p;
+        z_col_end   += p;
+    }
+}
+
+// x += gamma*z
+// Only the lower triangular part is read and written
+inline void sym_mat_update(
+    int p, double* x, const double* z, double gamma
+)
+{
+    double*       x_col_begin = x;
+    const double* x_col_end   = x + p;
+
+    const double* z_col_begin = z;
+    const double* z_col_end   = z + p;
+
+    for(int j = 0; j < p; j++)
+    {
+        x = x_col_begin + j;
+        z = z_col_begin + j;
+
+        for(; x < x_col_end; x++, z++)
+            (*x) += gamma * (*z);
+
+        x_col_begin += p;
+        x_col_end   += p;
+
+        z_col_begin += p;
+        z_col_end   += p;
+    }
+}
+
+// Frobenius norm of a symmetric matrix
+// Only the lower triangular part is read
+inline double sym_mat_norm(int p, const double* x)
+{
+    const double* col_begin = x;
+    const double* col_end   = x + p;
+    double diag = 0.0;
+    double off_diag = 0.0;
+
+    for(int j = 0; j < p; j++, col_begin += p, col_end += p)
+    {
+        x = col_begin + j;
+        diag += (*x) * (*x);
+        x = x + 1;
+
+        for(; x < col_end; x++)
+            off_diag += (*x) * (*x);
+    }
+
+    return std::sqrt(diag + 2 * off_diag);
+}
+
+// FPS objective function: -<S, X> + lambda * ||X||_1
+// Only the lower triangular part is read
+inline double fps_objfn(int p, const double* x, const double* s, double lambda)
+{
+    const double* x_col_begin = x;
+    const double* x_col_end   = x + p;
+
+    const double* s_col_begin = s;
+    const double* s_col_end   = s + p;
+
+    double diag1 = 0.0, diag2 = 0.0;
+    double off_diag1 = 0.0, off_diag2 = 0.0;
+
+    for(int j = 0; j < p; j++)
+    {
+        x = x_col_begin + j;
+        s = s_col_begin + j;
+
+        diag1 += (*s) * (*x);
+        diag2 += std::abs(*x);
+
+        x = x + 1;
+        s = s + 1;
+
+        for(; x < x_col_end; x++, s++)
+        {
+            off_diag1 += (*s) * (*x);
+            off_diag2 += std::abs(*x);
+        }
+
+        x_col_begin += p;
+        x_col_end   += p;
+
+        s_col_begin += p;
+        s_col_end   += p;
+    }
+
+    return -(diag1 + off_diag1 * 2) + lambda * (diag2 + off_diag2 * 2);
+}
+
 
 #endif  // FASTFPS_FASTFPS_H
