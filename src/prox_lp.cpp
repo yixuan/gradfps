993bfec49fa4bd732deef19b5c3e18582985ac82
Yixuan Qiu <yixuanq@gmail.com>
2019-03-07 15:03:42 -0500

    faster convergence

--- src/prox_lp.cpp
+++ src/prox_lp.cpp
@@ -1,28 +1,32 @@
 #include "common.h"
 #include <boost/math/tools/roots.hpp>
+#include <boost/fusion/tuple.hpp>
 
 using Rcpp::NumericVector;
 using Rcpp::NumericMatrix;
 
 // Solve f(x) = c * x^(p - 1) + x = v, c > 0, 1 < p < 2, x > 0, v > 0
 // f'(x) = (p - 1) * c * x^(p - 2) + 1
+// f''(x) = (p - 1) * (p - 2) * c * x^(p - 3)
 class sub_equation1
 {
 private:
     const double m_c;
     const double m_pm1;
+    const double m_pm1c;
     const double m_v;
 
 public:
     sub_equation1(double c, double p, double v) :
-        m_c(c), m_pm1(p - 1.0), m_v(v)
+        m_c(c), m_pm1(p - 1.0), m_pm1c(m_pm1 * c), m_v(v)
     {}
 
     std::pair<double, double> operator()(const double& x) const
     {
         const double xpm1 = std::pow(x, m_pm1);
+        const double xpm2 = xpm1 / x;
         const double fx = m_c * xpm1 + x - m_v;
-        const double dx = m_pm1 * m_c * xpm1 / x + 1.0;
+        const double dx = m_pm1c * xpm2 + 1.0;
         // Rcpp::Rcout << "eq1: " << fx << std::endl;
         return std::make_pair(fx, dx);
     }
@@ -30,25 +34,28 @@ public:
 
 // Solve g(u) = u^r + c * u = v, c > 0, r > 1, u > 0, v > 0
 // g'(u) = r * u^(r - 1) + c
+// g''(u) = r * (r - 1) * u^(r - 2)
 class sub_equation2
 {
 private:
     const double m_c;
     const double m_r;
+    const double m_rr1;
     const double m_v;
 
 public:
     sub_equation2(double c, double r, double v) :
-        m_c(c), m_r(r), m_v(v)
+        m_c(c), m_r(r), m_rr1(r * (r - 1.0)), m_v(v)
     {}
 
-    std::pair<double, double> operator()(const double& u) const
+    boost::fusion::tuple<double, double, double> operator()(const double& u) const
     {
         const double urm1 = std::pow(u, m_r - 1.0);
         const double gu = urm1 * u + m_c * u - m_v;
         const double du = m_r * urm1 + m_c;
+        const double d2u = m_rr1 * urm1 / u;
         // Rcpp::Rcout << "eq2: u = " << u << ", gu = " << gu << std::endl;
-        return std::make_pair(gu, du);
+        return boost::fusion::make_tuple(gu, du, d2u);
     }
 };
 
@@ -84,7 +91,7 @@ inline double solve_equation(double c, double p, double v)
     const double ub = 1.0;
     const int digits = 30;
     boost::uintmax_t maxit = 30;
-    const double res = boost::math::tools::newton_raphson_iterate(sub_equation2(c, r, v), guess, lb, ub, digits, maxit);
+    const double res = boost::math::tools::schroder_iterate(sub_equation2(c, r, v), guess, lb, ub, digits, maxit);
     return std::pow(res, r);
 }
 
