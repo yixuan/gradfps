a95d4e0853158e807253ec95882375a684db8ab6
Yixuan Qiu <yixuanq@gmail.com>
2019-03-08 13:42:23 -0500

    benchmarking code

--- src/gradfps_prox.cpp
+++ src/gradfps_prox.cpp
@@ -29,7 +29,8 @@ inline void add_soft_threshold(const MatrixXd& x, double penalty, MatrixXd& res)
 
 // [[Rcpp::export]]
 List gradfps_prox(MapMat S, MapMat x0, int d, double lambda,
-                  double lr, int maxiter, double eps_abs, double eps_rel,
+                  double lr = 0.001, int maxiter = 500,
+                  double eps_abs = 1e-3, double eps_rel = 1e-3,
                   bool verbose = true)
 {
     // Dimension of the covariance matrix
@@ -65,6 +66,65 @@ List gradfps_prox(MapMat S, MapMat x0, int d, double lambda,
     }
 
     return List::create(
+        Rcpp::Named("projection") = 0.5 * (z1 + z2),
+        Rcpp::Named("z1") = z1,
+        Rcpp::Named("z2") = z2
+    );
+}
+
+// [[Rcpp::export]]
+List gradfps_prox_benchmark(MapMat S, MapMat x0, MapMat Pi, int d, double lambda,
+                  double lr = 0.001, int maxiter = 500,
+                  double eps_abs = 1e-3, double eps_rel = 1e-3,
+                  bool verbose = true)
+{
+    // Dimension of the covariance matrix
+    const int n = S.rows();
+    const int p = S.cols();
+    if(n != p)
+        Rcpp::stop("S must be square");
+
+    MatrixXd z1 = x0, z2 = x0, zdiff(p, p), newz1(p, p);
+    SpMat l1(p, p);
+
+    // Metrics in each iteration
+    std::vector<double> errs;
+    std::vector<double> times;
+    double t1, t2;
+
+    for(int i = 0; i < maxiter; i++)
+    {
+        if(i % 50 == 0)
+            Rcpp::Rcout << "iter = " << i << std::endl;
+
+        t1 = get_wall_time();
+
+        // zdiff <- (z2 - z1) / 2
+        zdiff.noalias() = 0.5 * (z2 - z1);
+
+        // z1 <- -zdiff + prox_fantope(z2)
+        z2.noalias() += lr * S;
+        MapConstMat z2m(z2.data(), z2.rows(), z2.cols());
+        MapMat newz1m(newz1.data(), newz1.rows(), newz1.cols());
+        prox_fantope_impl(z2m, d, 5 * d, 10, newz1m);
+        newz1.noalias() -= zdiff;
+
+        // l1 <- soft_threshold(z1, lr * lambda)
+        // z2 <- zdiff + l1
+        z2.swap(zdiff);
+        add_soft_threshold(z1, lr * lambda, z2);
+
+        z1.swap(newz1);
+
+        t2 = get_wall_time();
+        times.push_back(t2 - t1);
+        errs.push_back((0.5 * (z1 + z2) - Pi).norm());
+    }
+
+    return List::create(
+        Rcpp::Named("projection") = 0.5 * (z1 + z2),
+        Rcpp::Named("errors") = errs,
+        Rcpp::Named("times") = times,
         Rcpp::Named("z1") = z1,
         Rcpp::Named("z2") = z2
     );
