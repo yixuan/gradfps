cee2096cab9367b751dabc60b16a8639f5ccd939
Yixuan Qiu <yixuanq@gmail.com>
2019-04-16 19:25:52 -0400

    add typedefs

--- src/common.h
+++ src/common.h
@@ -5,12 +5,17 @@
 
 typedef Eigen::MatrixXd MatrixXd;
 typedef Eigen::VectorXd VectorXd;
+
 typedef Eigen::Map<MatrixXd> MapMat;
 typedef Eigen::Map<VectorXd> MapVec;
 typedef Eigen::Map<const VectorXd> MapConstVec;
 typedef Eigen::Map<const MatrixXd> MapConstMat;
+
 typedef Eigen::Ref<VectorXd> RefVec;
 typedef Eigen::Ref<MatrixXd> RefMat;
+typedef Eigen::Ref<const VectorXd> RefConstVec;
+typedef Eigen::Ref<const MatrixXd> RefConstMat;
+
 typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Map<SpMat> MapSpMat;
 typedef Eigen::Map<const SpMat> MapConstSpMat;
