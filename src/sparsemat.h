0a7faf7543ec566d706f5d1aa890887f16d1fddf
Yixuan Qiu <yixuanq@gmail.com>
2020-03-28 21:53:55 -0400

    SIMD implementation of sparse matrix-vector multiplication

--- src/sparsemat.h
+++ src/sparsemat.h
@@ -1,8 +1,66 @@
 #ifndef GRADFPS_SPARSE_MAT_H
 #define GRADFPS_SPARSE_MAT_H
 
+#include <xsimd/xsimd.hpp>
 #include "common.h"
 #include "symmat.h"
+
+// Create a SIMD vector of x[ind[0]], x[ind[1]], x[ind[2]], ...
+template <int simd_size>
+inline xsimd::batch<double, simd_size> discontinuous_load(const double* x, const int* ind)
+{
+    alignas(simd_size * sizeof(double)) double data[simd_size];
+    for(int i = 0; i < simd_size; i++)
+        data[i] = x[ind[i]];
+    return xsimd::load_aligned(data);
+}
+
+template <int simd_size>
+inline void discontinuous_store_add(const xsimd::batch<double, simd_size>& v, double* x, const int* ind)
+{
+    alignas(simd_size * sizeof(double)) double data[simd_size];
+    v.store_aligned(data);
+    for(int i = 0; i < simd_size; i++)
+        x[ind[i]] += data[i];
+}
+
+// Specialization for 256-bit vector
+#ifdef __AVX__
+
+template <>
+inline xsimd::batch<double, 4> discontinuous_load<4>(const double* x, const int* ind)
+{
+    return xsimd::batch<double, 4>(x[ind[0]], x[ind[1]], x[ind[2]], x[ind[3]]);
+}
+
+template <>
+inline void discontinuous_store_add<4>(const xsimd::batch<double, 4>& v, double* x, const int* ind)
+{
+    x[ind[0]] += v[0];
+    x[ind[1]] += v[1];
+    x[ind[2]] += v[2];
+    x[ind[3]] += v[3];
+}
+
+// Specialization for 128-bit vector
+#elif defined(__SSE__)
+
+template <>
+inline xsimd::batch<double, 2> discontinuous_load<2>(const double* x, const int* ind)
+{
+    return xsimd::batch<double, 2>(x[ind[0]], x[ind[1]]);
+}
+
+template <>
+inline void discontinuous_store_add<2>(const xsimd::batch<double, 2>& v, double* x, const int* ind)
+{
+    x[ind[0]] += v[0];
+    x[ind[1]] += v[1];
+}
+
+#endif
+
+
 
 // Define a structure that mimics the dgCMatrix class in R
 // Assume that the matrix is lower-triangular
@@ -113,6 +171,9 @@ public:
         // MapVec      y(y_out, m_n);
         // y.noalias() = to_spmat().selfadjointView<Eigen::Lower>() * x;
 
+        typedef xsimd::batch<double, xsimd::simd_type<double>::size> vec;
+        const int simd_size = xsimd::simd_type<double>::size;
+
         // Zero out y vector
         std::fill(y_out, y_out + m_n, 0.0);
 
@@ -141,7 +202,19 @@ public:
             }
 
             // Off-diagonal elements
+            const int len = col_end - elem_ptr;
+            const int vec_size = len - len % simd_size;
+            const double* simd_end = elem_ptr + vec_size;
+            vec x_in_col_simd = xsimd::set_simd(x_in_col);
             double y_out_col = 0.0;
+            for( ; elem_ptr < simd_end; elem_ptr += simd_size, row_id_ptr += simd_size)
+            {
+                vec elem_simd = xsimd::load_unaligned(elem_ptr);
+                vec prod = elem_simd * x_in_col_simd;
+                discontinuous_store_add<simd_size>(prod, y_out, row_id_ptr);
+                vec x_in_row_simd = discontinuous_load<simd_size>(x_in, row_id_ptr);
+                y_out_col += xsimd::hadd(elem_simd * x_in_row_simd);
+            }
             for( ; elem_ptr < col_end; elem_ptr++, row_id_ptr++)
             {
                 // Add value to the y vector
